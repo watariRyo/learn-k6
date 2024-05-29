f696e12324830ab8b21c1deac19aea320d39150d
watariRyo <72895473+watariRyo@users.noreply.github.com>
2024-05-29 23:25:36 +0900

    init k6

---
+++ basic/stress-test.js
@@ -0,0 +1,28 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+  stages: [
+    {
+      duration: "5s",
+      target: 1000,
+    },
+    {
+      duration: "30s",
+      target: 1000,
+    },
+    {
+      duration: "5s",
+      target: 0,
+    },
+  ],
+};
+
+export default function () {
+  http.get("https://test.k6.io");
+  sleep(1);
+  http.get("https://test.k6.io/contacts.php");
+  sleep(2);
+  http.get("https://test.k6.io/news.php");
+  sleep(2);
+}
